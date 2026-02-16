e6a60c7dee094f8efad4b93fdf4871a03776b2dd
dz00te <dz00te@framassa.org>
2026-02-16 10:23:19 +0100

    Update PegaProx to 0.6.6

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=ghcr.io/dz00te/pegaprox-app:0.6.5" \
+    --label="org.nethserver.images=ghcr.io/dz00te/pegaprox-app:0.6.6" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
